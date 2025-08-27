002f775199803438d7e0385dd088cee18a7be34e
Chris <christian@saddai.de>
2025-08-27 11:35:27 +0200

    idk

--- hosts/laptop.nix
+++ hosts/laptop.nix
@@ -1,10 +1,4 @@
 { config, pkgs, lib, ... }:
 {
   networking.hostName = "Chris-laptop";
-
-  home-manager = {
-    useGlobalPkgs = true;
-    useUserPackages = true;
-    users.chris = import ./home/work-home.nix; 
-  };
 }
